5f59252f3c444ae1d709f0d1c4d8c15a3f0d96f2
Masavi <mauriciosaavedra2005@gmail.com>
2023-06-22 13:42:36 -0600

    feat: add the 'CRUD with Express' lecture files

--- m03/w06/express-crud/server.js
+++ m03/w06/express-crud/server.js
@@ -1,97 +1,152 @@
 const express = require('express');
-const morgan = require('morgan');
+const { uid } = require('uid');
 const app = express();
-const PORT = 5000;
-
-app.use(express.urlencoded({ extended: true })); // Handles data in regular HTML form submission (req.body)
-app.use(morgan('dev')); // Logs each received request into the console
-app.set('view engine', 'ejs'); // to set templates view engine (ejs)
+const PORT = 4000;
 
 /**
  * DATABASE
  */
-
-const moviesDatabase = [
-  { id: 1, title: 'Lost in Translation', director: 'Sofia Coppola' },
-  { id: 2, title: 'The Godfather', director: 'Francis Ford Coppola' },
-]
+const database = [
+  { id: '1', title: 'The Godfather', director: 'Francis Copolla' },
+  { id: '2', title: 'Lost in Translation', director: 'Sofia Copolla' },
+];
 
 /**
- * RENDERING ROUTES
- * designed to allow the user to fetch html+css+js to show an interface on the browser
+ * MIDDLEWARES
+ */
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+/**
+ * ROUTES
+ * 1. help you navigate different sections of a website
+ * 2. help you access data, mainly from a database
+ * Enable us to respond back to a client request, however we feel like
  */
 
-// Homepage
 app.get('/', (req, res) => {
   res.render('index');
 })
 
-// Browse Movies
+/**
+ * RENDER ROUTES (VIEWS)
+ */
+
+// List all the movies (READ)
 app.get('/movies', (req, res) => {
-  const templateVars = {
-    movies: moviesDatabase
-  };
-
-  res.render('movies-show', templateVars);
+  const templateVars = { movies: database }
+  res.render('movies', templateVars);
 })
 
-// Look at the info of one single Movie
+// Show one movie (READ)
 app.get('/movies/:idMovie', (req, res) => {
-  const { idMovie } = req.params;
-  const foundMovie = moviesDatabase.find(movie => movie.id === Number(idMovie));
-  const templateVars = { movie: foundMovie };
-  res.render('movie-show', templateVars);
+  const id = req.params.idMovie;
+  const foundMovie = database.find((movie) => movie.id === id)
+  const templateVars = { movie: foundMovie }
+  res.render('movie', templateVars);
 })
 
-// Shows the form to edit a new Movie
-app.get('/movies/:idMovie/edit', (req, res) => {
-  const { idMovie } = req.params;
-  const foundMovie = moviesDatabase.find(movie => movie.id === Number(idMovie));
-  const templateVars = { movie: foundMovie };
-  res.render('movie-edit', templateVars);
-})
-
-// Shows the form to edit a new Movie
-app.get('/movies-new', (req, res) => {
+// Show form to add new movie (READ)
+app.get('/movie-new', (req, res) => {
   res.render('movie-new');
 })
 
+// Show form to edit one movie (READ)
+app.get('/movie-edit/:idMovie', (req, res) => {
+  const id = req.params.idMovie;
+  const foundMovie = database.find((movie) => movie.id === id)
+  const templateVars = { movie: foundMovie }
+  res.render('movie-edit', templateVars);
+})
+
 /**
- * API ROUTES
- * designed to allow clients to fetch or manipulate data
+ * API ROUTES (DATA)
+ * CRUD OPERATIONS
+ * (CREATE, READ, UPDATE, DELETE)
  */
 
-app.post('/api/movies/new', (req, res) => {
+// CREATE
+app.post('/api/movies', (req, res) => {
+  // const title = req.body.title;
+  // const director = req.body.director;
   const { title, director } = req.body;
-  const newMovie = { id: (moviesDatabase.length + 1), title, director };
-  moviesDatabase.push(newMovie);
-  console.log(moviesDatabase);
-  res.redirect(`/movies/${newMovie.id}`);
+
+  const newMovie = {
+    id: uid(3),
+    // title: title,
+    title,
+    // director: director,
+    director,
+  }
+
+  database.push(newMovie);
+
+  console.log('database', database);
+
+  // This is to send the raw data
+  // res.json(newMovie);
+
+  // This is to redirect the user to the "browse movies" view
+  res.redirect('/movies');
 })
 
+// READ (ALL)
+app.get('/api/movies', (req, res) => {
+  res.json({ movies: database })
+})
+
+// READ (ONE)
+app.get('/api/movies/:idMovie', (req, res) => {
+  const id = req.params.idMovie;
+  const foundMovie = database.find((movie) => movie.id === id)
+  res.json({ movie: foundMovie })
+})
+
+// UPDATE (ONE)
 app.post('/api/movies/:idMovie/edit', (req, res) => {
-  const { idMovie } = req.params;
-  const { title, director } = req.body;
+  const title = req.body.title;
+  const director = req.body.director;
 
-  const foundMovie = moviesDatabase.find(movie => movie.id === Number(idMovie));
-  foundMovie.title = title;
-  foundMovie.director = director;
+  const id = req.params.idMovie;
 
-  res.redirect(`/movies/${idMovie}`);
-})
+  const updatedMovie = {
+    id,
+    title,
+    director,
+  }
 
-app.post('/api/movies/:idMovie/delete', (req, res) => {
-  const { idMovie } = req.params;
-
-  moviesDatabase.forEach((movie, index) => {
-    if (movie.id === Number(idMovie)) {
-      console.log('found');
-      moviesDatabase.splice(index, 1);
-      console.log(moviesDatabase);
+  database.forEach((movie, index) => {
+    if (movie.id === id) {
+      database[index] = updatedMovie;
     }
-  });
+  })
 
-  res.redirect(`/movies`);
+  // This sends the raw data
+  // res.json({ movie: updatedMovie });
+
+  // This redirects to another view
+  res.redirect('/movies')
 })
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+// DELETE (ONE)
+app.get('/api/movies/:idMovie/delete', (req, res) => {
+  const id = req.params.idMovie;
+
+  database.forEach((movie, index) => {
+    if (movie.id === id) {
+      database.splice(index, 1);
+    }
+  })
+
+  // This is for sending raw data
+  // res.json({ message: 'movie has been deleted!' });
+
+  // This is to redirect the user to another view
+  res.redirect('/movies')
+})
+
+
+/**
+ * LISTEN METHOD
+ */
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
